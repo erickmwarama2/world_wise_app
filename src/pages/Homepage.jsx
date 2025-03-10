ef74dda7dabce888deb1e7284076afb459d7f72c
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-03-10 11:43:26 +0300

    initialize project

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+// import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      {/* <Link to="/pricing">Pricing</Link> */}
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default Homepage;
